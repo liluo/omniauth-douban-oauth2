fb4f39cc5c3b7c96836122a3fba898547bc99d26
luo <i@liluo.org>
2012-07-09 02:17:16 +0800

    bug test

--- lib/omniauth/strategies/douban.rb
+++ lib/omniauth/strategies/douban.rb
@@ -8,38 +8,35 @@ module OmniAuth
         :authorize_url => 'https://www.douban.com/service/auth2/auth',
         :token_url     => 'https://www.douban.com/service/auth2/token'
       }
-      options :token_params, {
-        :parse         => :json
-      }
 
-      uid do
-        raw_info['id']
-      end
-
-      info do
-        {
-          :nickname    => raw_info['db:uid']['$t'],
-          :name        => raw_info['title']['$t']
-          :location    => raw_info['location'] ? raw_info['location']['$t'] : nil,
-          :image       => raw_info['link'].find{ |l| l['@rel'] == 'icon' }['@href'],
-          :description => raw_info['content']['$t'],
-          :urls        => {
-            'Douban' => raw_info['link'].find{ |l| l['@rel'] == 'alternate' }['@href']
-          }
-        }
-      end
-
-      extra do
-        { :raw_info => raw_info }
-      end
-
-      def raw_info
-        access_token.options[:mode]       = :query
-        access_token.options[:param_name] = 'access_token'
-        @raw_info ||= access_token.get('/people/%40me?alt=json').body
-      # rescue ::Errno::ETIMEDOUT
-      #   raise ::Timeout::Error
-      end
+#      uid do
+#        raw_info['id']
+#      end
+#
+#      info do
+#        {
+#          :nickname    => raw_info['db:uid']['$t'],
+#          :name        => raw_info['title']['$t']
+#          :location    => raw_info['location'] ? raw_info['location']['$t'] : nil,
+#          :image       => raw_info['link'].find{ |l| l['@rel'] == 'icon' }['@href'],
+#          :description => raw_info['content']['$t'],
+#          :urls        => {
+#            'Douban' => raw_info['link'].find{ |l| l['@rel'] == 'alternate' }['@href']
+#          }
+#        }
+#      end
+#
+#      extra do
+#        { :raw_info => raw_info }
+#      end
+#
+#      def raw_info
+#        access_token.options[:mode]       = :query
+#        access_token.options[:param_name] = 'access_token'
+#        @raw_info ||= access_token.get('/people/%40me?alt=json').body
+#      rescue ::Errno::ETIMEDOUT
+#        raise ::Timeout::Error
+#      end
 
     end
   end
